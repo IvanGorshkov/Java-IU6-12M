a2fd99bb9be1a7a44ea7d682caeb02b922ecdd6b
Ivan Gorshkov <ivanagorshkov160@gmail.com>
2023-02-21 00:00:04 +0300

    Задание 2

---
+++ Lab1/Z2/src/com/java/lab/Main.java
@@ -0,0 +1,18 @@
+package com.java.lab;
+
+class Hello {
+    public static void welcomeWith(String[] args) {
+        System.out.println("Oh, WELCOME!");
+        for (String arg: args) {
+            System.out.println(arg + "");
+        }
+    }
+}
+
+// Создать класс Hello, который будет приветствовать любого пользователя, используя командную строку.
+public class Main {
+
+    public static void main(String[] args) {
+        Hello.welcomeWith(args);
+    }
+}
