6fdafd6978726a994f0e44e5744786bcf51e7bbf
Ivan Gorshkov <ivanagorshkov160@gmail.com>
2023-04-01 19:38:16 +0300

    Задание 3 Лаб 7

---
+++ Lab7/Z3/src/com/java/lab/Main.java
@@ -0,0 +1,22 @@
+package com.java.lab;
+import java.util.HashSet;
+
+
+// Напечатать без повторения слова текста, у которых первая и последняя буквы совпадают.
+public class Main {
+    public static void main(String[] args) {
+        var text = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
+        var set = new HashSet<>();
+        var words = text.split(" ");
+
+        for (var word : words) {
+            if (word.charAt(0) == word.charAt(word.length() - 1)) {
+                set.add(word);
+            }
+        }
+
+        for (var word : set) {
+            System.out.print(word + " ");
+        }
+    }
+}
